d8b1047fb091ab6f060aacf41e12704e3d145285
tiresc <tiresandplanes@gmail.com>
2020-06-14 17:18:39 -0400

    Remove feed struct

--- parse.h
+++ parse.h
@@ -1,20 +1,9 @@
-struct feed {
-	char *rss_url;
-	char **titles;
-	char **links;
-	char *find_titles; //searches each line for titles
-	char *find_links; //searches each line for links
-	int amount_of_titles;
-    int amount_of_links;
-	struct rss_tags* d;
-};	
 struct rss_tags {
-
 	char *title;
 	char *link;
 	char *description;
 	char *mp3_link;
-	struct Node* next;
+	struct rss_tags* next;
 };	
 
 struct MemoryStruct {
